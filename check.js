47e6262c7ee88a1eb0aafe6963b2569497fbc265
sanskaryo <sanskartodwal@gmail.com>
2024-12-25 16:09:11 +0530

    eradicated some issues

--- check.js
+++ check.js
@@ -1,4 +1,4 @@
 require('dotenv').config();
 
-console.log(process.env.JWT_ADMIN_PASSWORD);  // should output "adminpassword"
-console.log(process.env.MONGO_URL);  // should output your MongoDB URL
+console.log(process.env.JWT_ADMIN_PASSWORD);  
+console.log(process.env.MONGO_URL);  
